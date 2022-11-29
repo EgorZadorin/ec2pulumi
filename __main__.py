9e355b0756dab0bd44d8a1e9d479588df109e720
EgorZadorin <egozador@yandex.ru>
2022-11-29 15:21:54 +0100

    sample ELB

--- __main__.py
+++ __main__.py
@@ -1,44 +1,96 @@
+"""An AWS Python Pulumi program"""
+
 import pulumi
 import pulumi_aws as aws
 
-size = 't2.micro'
 
-ami = aws.get_ami(most_recent=True,
-                  owners=["137112412989"],
-                  filters=[aws.GetAmiFilterArgs(name="name", values=["amzn-ami-hvm-*"])])
+ami = aws.get_ami(
+    most_recent=True,
+    owners=["137112412989"],
+    filters=[{"name": "name", "values": ["amzn-ami-hvm-*-x86_64-ebs"]}],
+)
 
-group = aws.ec2.SecurityGroup('web-secgrp',
-    description='Enable HTTP access',
-    ingress=[aws.ec2.SecurityGroupIngressArgs(
-        protocol='tcp',
-        from_port=80,
-        to_port=80,
-        cidr_blocks=['0.0.0.0/0'],
-    )])
+vpc = aws.ec2.get_vpc(default=True)
+vpc_subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 
-user_data1 = """
-#!/bin/bash
-echo "server1" > index.html
+group = aws.ec2.SecurityGroup(
+    "web-secgrp",
+    description="Enable HTTP Access",
+    vpc_id=vpc.id,
+    ingress=[
+        {
+            "protocol": "icmp",
+            "from_port": 8,
+            "to_port": 0,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 80,
+            "to_port": 80,
+            "cidr_blocks": ["0.0.0.0/0"],
+        },
+    ],
+    egress=[
+        {
+            "protocol": "tcp",
+            "from_port": 80,
+            "to_port": 80,
+            "cidr_blocks": ["0.0.0.0/0"],
+        }
+    ],
+)
+
+lb = aws.lb.LoadBalancer(
+    "loadbalancer",
+    internal=False,
+    security_groups=[group.id],
+    subnets=vpc_subnets.ids,
+    load_balancer_type="application",
+)
+
+target_group = aws.lb.TargetGroup(
+    "target-group", port=80, protocol="HTTP", target_type="ip", vpc_id=vpc.id
+)
+
+listener = aws.lb.Listener(
+    "listener",
+    load_balancer_arn=lb.arn,
+    port=80,
+    default_actions=[{"type": "forward", "target_group_arn": target_group.arn}],
+)
+
+
+ips = []
+hostnames = []
+
+for az in aws.get_availability_zones().names:
+    server = aws.ec2.Instance(
+        f"web-server-{az}",
+        instance_type="t2.micro",
+        vpc_security_group_ids=[group.id],
+        ami=ami.id,
+        user_data="""#!/bin/bash
+echo \"Hello, World -- from {}!\" > index.html
 nohup python -m SimpleHTTPServer 80 &
-"""
+""".format(
+            az
+        ),
+        tags={
+            "Name": "web-server",
+        },
+    )
+    ips.append(server.public_ip)
+    hostnames.append(server.public_dns)
 
-user_data2 = """
-#!/bin/bash
-echo "server2" > index.html
-nohup python -m SimpleHTTPServer 80 &
-"""
+    attachment = aws.lb.TargetGroupAttachment(
+        f"web-server-{az}",
+        target_group_arn=target_group.arn,
+        target_id=server.private_ip,
+        port=80,
+    )
 
-server1 = aws.ec2.Instance('web-server1-www',
-    instance_type=size,
-    vpc_security_group_ids=[group.id],
-    user_data=user_data1,
-    ami=ami.id)
 
-server2 = aws.ec2.Instance('web-server2-www',
-    instance_type=size,
-    vpc_security_group_ids=[group.id],
-    user_data=user_data2,
-    ami=ami.id)
-
-pulumi.export('server1', server1.public_dns)
-pulumi.export('server2', server2.public_dns)
+pulumi.export("ips", ips)
+pulumi.export("hostnames", hostnames)
+pulumi.export("url", lb.dns_name)
